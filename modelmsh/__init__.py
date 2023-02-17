34719e85952e83f93edeedd706014a735386fb5e
pcachim <pcachim@ua.pt>
2023-02-17 21:27:56 +0100

    20230217.2127

---
+++ modelmsh/__init__.py
@@ -0,0 +1,5 @@
+"""modelmsh: A Python package for the generation of 2D and 3D meshes for finite element analysis.
+with the following features:
+    read and write mesh files in the following formats:
+        cgns, gmsh, med, medit, msh, nastran, stl, vtk, vtu
+"""
